d06534fe26195342494575e89760cc9c4ae13f3c
André Wendt <andre.wendt@infopark.de>
2011-06-08 22:44:59 +0200

    Initial commit

---
+++ kerryb.js
@@ -0,0 +1,3 @@
+/*jslint white: true, evil: true,browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true, strict: true */
+/*global window: false, jQuery: false */
+"use strict";
